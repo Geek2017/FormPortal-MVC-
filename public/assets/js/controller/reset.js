1751887fba41ed825370776508da2a500e12bd22
Geek2017 <jmlgrefaldo@bicol-u.edu.ph>
2019-11-07 03:58:14 +0800

    Auth Full Cycle

--- public/assets/js/controller/reset.js
+++ public/assets/js/controller/reset.js
@@ -1,14 +1,18 @@
-var email = document.querySelector('#resetloginEmail').value;
-                          
-$("#resetEmail").on("submit", function (e) {
+$(document).ready(function () {
+   
+    var email = document.querySelector('#resetloginEmail').value;
+
+    $("#resetEmail").on("submit", function (e) {
         e.preventDefault();
 
         const email = $('#resetloginEmail').val();
         console.log(email);
-        
+
         firebase.auth().sendPasswordResetEmail(email)
-        .then(function() {
-            alert('Reset link has been sent to provided email address');
-            window.location.replace("./index.html");
-        })
+            .then(function () {
+                alert('Reset link has been sent to provided email address');
+                window.location.replace("login.html");
+            })
     });
+    
+});
